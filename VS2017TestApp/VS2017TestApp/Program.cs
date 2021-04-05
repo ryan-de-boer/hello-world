bc6309944e4d2f69f83690571e97890e7b41117a
ryan-de-boer <ryan.l.de.boer@gmail.com>
2021-04-05 16:27:48 +0800

    Update Program.cs

--- VS2017TestApp/VS2017TestApp/Program.cs
+++ VS2017TestApp/VS2017TestApp/Program.cs
@@ -16,7 +16,7 @@ namespace VS2017TestApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form12());
+            Application.Run(new Form1());
         }
     }
 }
